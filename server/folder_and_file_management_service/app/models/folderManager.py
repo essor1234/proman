5499f4eef1d7c5def9ad03ea4b94c311af7d0d29
essor1234 <121819259+essor1234@users.noreply.github.com>
2025-10-22 15:39:06 +0700

    Add --hot_reload for docker running/ Build basic structure/ Add base class for file/ Test create sqlite database

---
+++ server/folder_and_file_management_service/app/models/folderManager.py
@@ -0,0 +1,8 @@
+
+
+
+class FolderManager:
+    def __init__(self, id: str, folderId: str, projectid: str):
+        self.id = id
+        self.folderId = folderId
+        self.projectid = projectid
