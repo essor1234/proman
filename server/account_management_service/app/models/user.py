78c9577dbbef8880a1e63956924ce4ceead0f692
KhangVo09 <121435621+KhangVo09@users.noreply.github.com>
2025-11-05 10:26:45 +0700

    create a login and microservice that have a function to get user so that my team can get the user info

--- server/account_management_service/app/models/user.py
+++ server/account_management_service/app/models/user.py
@@ -1,10 +1,9 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
-from datetime import datetime
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    userName: str
-    email: str = Field(unique=True, index=True)
-    password: str
-    created_at: datetime = Field(default_factory=datetime.utcnow)
+    username: str
+    email: str
+    full_name: Optional[str] = None
+    hashed_password: str  # store hashed password here
