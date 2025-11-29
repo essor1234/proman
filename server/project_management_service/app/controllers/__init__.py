28aba50f2e4146bbc647586f30f7b42c0cc8912b
Ericthai279 <phucth2709@gmail.com>
2025-11-29 16:24:06 +0700

    Feature/project management service (#96)
    * Phuc First Commit
    
    * Phuc First Commit
    
    * Second Commit
    
    * Create-Read function
    
    * Finished CRUD
    
    * Finished CRUD (error free)
    
    * Complete rebase
    
    * Merge Main
    
    * Restructured to follow Microservice principles
    
    * Merge with main branch
    
    * Merge with main
    
    * Fix docker
    
    * Split API
    
    * reconfig database.py in core
    
    * fix main
    
    * fix not running on port 8004
    
    * connect to account_management_service and getting userID
    
    * Linking with group sevice - Not Done
    
    * Merge with main
    
    * Merge with main
    
    * Fixing group management -Working only Post Method
    
    * Fixed group functions for group management by turning uuid into strings
    
    * Fixed group and member functions in group_managements
    
    * Finish linking group and project service
    
    * Finish Linking group and project
    
    * Resolve conflict
    
    * rm .db
    
    * rm .db
    
    * Fix __init__ controller
    
    * Fix .gitignore

--- server/project_management_service/app/controllers/__init__.py
+++ server/project_management_service/app/controllers/__init__.py
@@ -3,15 +3,15 @@
 
 
 
-from .group import (
-    create_group, add_member, get_groups, get_group, get_members,
-    update_group, delete_group, remove_member
-)
+# from .group import (
+#     create_group, add_member, get_groups, get_group, get_members,
+#     update_group, delete_group, remove_member
+# )
 
-__all__ = [
-    "create_group", "add_member", "get_groups", "get_group", "get_members",
-    "update_group", "delete_group", "remove_member"
-]
+# __all__ = [
+#     "create_group", "add_member", "get_groups", "get_group", "get_members",
+#     "update_group", "delete_group", "remove_member"
+# ]
 
 from .project import (
     create_project, add_member, get_projects, get_project, get_members,
